03ecc3825be95d233a36f661b7d2ea42efdc8a09
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-11-27 20:20:00 -0300

    Create exemplo3.py

---
+++ Aula 3/exemplo3.py
@@ -0,0 +1,12 @@
+# Criar um programa de cálculo de desconto. O programa deverá receber o valor da compra, a idade do cliente e a cidade do cliente. Ao final o programa deverá exibir o valor do desconto aplicado na compra e o novo valor atualizado. Os critérios de desconto são:
+
+#1: Se o cliente tiver mais que 30 anos de idade - Aplicar 5% de desconto
+
+#2: Se o cliente for da cidade Fortaleza - Aplicar 5% de desconto
+#   Se o cliente for da cidade Maranguape - Aplicar 10% de desconto
+#   Se o cliente for da cidade Morada Nova - Aplicar 20% de desconto
+
+#3: Se o valor total da compra superar 1000 - Aplicar 10% de desconto
+#   Se o valor total da compra superar 2000 - Aplicar 15% de desconto
+
+# Os descontos devem ser acumulativos e será a soma dos descontos aplicados.
